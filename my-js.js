fdf594979f57cfb03b548b05476d3b172296e025
afunwa <105121911+afunwa505@users.noreply.github.com>
2022-05-30 10:25:26 +0100

    Add files via upload

--- my-js.js
+++ my-js.js
@@ -7,3 +7,18 @@ function mouseou(){
     document.querySelector('.product ol').style.display="none";
     document.querySelector('nav').style.height="120px";
 }
+var slideIndex = 0;
+showSlides ();
+function showSlides (){
+    var i;
+    var slides = document.getElementsByClassName('myslides');
+    for(i = 0; i< slides.length; i++) {
+        slides[i].style.display = 'none';
+    }
+    slideIndex++;
+    if(slideIndex >slides.length){
+        slideIndex = 1;
+    }
+    slides[slideIndex-1].style.display = 'block';
+    setTimeout(showSlides,5000);
+}
