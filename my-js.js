7ccb249963fa1498dd1e2258a09352933746536c
afunwa <afunwaemmanuel505@gmail.com>
2023-03-01 15:58:15 +0100

    Update my-js.js

--- my-js.js
+++ my-js.js
@@ -40,4 +40,4 @@ function showSlide (){
 }
 
 const d = new Date();
-document.getElementById("show_date").innerHTML = d.getFullYear();
+document.getElementByTagName('footer').innerHTML = d.getFullYear();
